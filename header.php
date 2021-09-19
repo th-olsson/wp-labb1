4cb80177fcc79478de014cc6d7094250d18f72bf
Thomas Olsson <olss.thom@gmail.com>
2021-09-20 01:46:56 +0200

    header menu and search form

--- header.php
+++ header.php
@@ -18,13 +18,7 @@
                         <a class="logo" href="index.html">Labb 1</a>
                     </div>
                     <div class="col-sm-6 hidden-xs">
-                        <form id="searchform" class="searchform">
-                            <div>
-                                <label class="screen-reader-text">Sök efter:</label>
-                                <input type="text" />
-                                <input type="submit" value="Sök" />
-                            </div>
-                        </form>
+                        <?php get_search_form() ?>
                     </div>
                     <div class="col-xs-4 text-right visible-xs">
                         <div class="mobile-menu-wrap">
@@ -37,42 +31,23 @@
         </header>
 
         <div class="mobile-search">
-            <form id="searchform" class="searchform">
-                <div>
-                    <label class="screen-reader-text">Sök efter:</label>
-                    <input type="text" />
-                    <input type="submit" value="Sök" />
-                </div>
-            </form>
+            <?php get_search_form() ?>
         </div>
 
         <nav id="nav">
             <div class="container">
                 <div class="row">
                     <div class="col-xs-12">
-                        <ul class="menu">
-                            <li class="current-menu-item">
-                                <a href="index.html">Hem</a>
-                            </li>
-                            <li>
-                                <a href="blogg.html">Blogg</a>
-                            </li>
-                            <li>
-                                <a href="undersida.html">Undersida</a>
-                            </li>
-                            <li>
-                                <a href="undersida2.html">Undersida 2</a>
-                            </li>
-                            <li>
-                                <a href="undersida3.html">Undersida 3</a>
-                            </li>
-                            <li>
-                                <a href="undersida4.html">Undersida 4</a>
-                            </li>
-                            <li>
-                                <a href="kontakt.html">Kontakt</a>
-                            </li>
-                        </ul>
+                        <?php
+                        wp_nav_menu(
+                            array(
+                                'menu' => 'nav',
+                                'container' => '',
+                                'theme_location' => 'nav',
+                                'menu_class' => 'menu',
+                            )
+                        );
+                        ?>
                     </div>
                 </div>
             </div>
