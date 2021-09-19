dbdf1f4571f34c862f4cb18c373955478952c9c7
Thomas Olsson <olss.thom@gmail.com>
2021-09-19 19:01:59 +0200

    removes header links

--- header.php
+++ header.php
@@ -4,10 +4,6 @@
 <head>
     <meta charset="UTF-8" />
     <title>Labb 1</title>
-    <link href="css/font-awesome.css" rel="stylesheet" type="text/css" />
-    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
-    <link href="css/style.css" rel="stylesheet" type="text/css" />
-    <script src="js/jquery.js"></script>
     <?php wp_head() ?>
 </head>
 
