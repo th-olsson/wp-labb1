b0154501f431d49945b79b634a882192dfa2ffe2
Thomas Olsson <olss.thom@gmail.com>
2021-09-19 08:17:50 +0200

    header and footer

---
+++ header.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Labb 1</title>
+    <link href="css/font-awesome.css" rel="stylesheet" type="text/css" />
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <script src="js/jquery.js"></script>
+</head>
+
+<body>
+
+    <div id="wrap">
+
+        <header id="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-8 col-sm-6">
+                        <a class="logo" href="index.html">Labb 1</a>
+                    </div>
+                    <div class="col-sm-6 hidden-xs">
+                        <form id="searchform" class="searchform">
+                            <div>
+                                <label class="screen-reader-text">Sök efter:</label>
+                                <input type="text" />
+                                <input type="submit" value="Sök" />
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-xs-4 text-right visible-xs">
+                        <div class="mobile-menu-wrap">
+                            <i class="fa fa-search"></i>
+                            <i class="fa fa-bars menu-icon"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <div class="mobile-search">
+            <form id="searchform" class="searchform">
+                <div>
+                    <label class="screen-reader-text">Sök efter:</label>
+                    <input type="text" />
+                    <input type="submit" value="Sök" />
+                </div>
+            </form>
+        </div>
+
+        <nav id="nav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <ul class="menu">
+                            <li class="current-menu-item">
+                                <a href="index.html">Hem</a>
+                            </li>
+                            <li>
+                                <a href="blogg.html">Blogg</a>
+                            </li>
+                            <li>
+                                <a href="undersida.html">Undersida</a>
+                            </li>
+                            <li>
+                                <a href="undersida2.html">Undersida 2</a>
+                            </li>
+                            <li>
+                                <a href="undersida3.html">Undersida 3</a>
+                            </li>
+                            <li>
+                                <a href="undersida4.html">Undersida 4</a>
+                            </li>
+                            <li>
+                                <a href="kontakt.html">Kontakt</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
