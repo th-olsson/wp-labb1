957dc9256f58543393d3d32b2af0bc93ae806000
Thomas Olsson <olss.thom@gmail.com>
2021-09-19 18:46:19 +0200

    separates front page and blog

---
+++ front-page.php
@@ -0,0 +1,21 @@
+<?php get_header() ?>
+
+<main>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="hero">
+                        <img src="<?php echo get_template_directory_uri()  . '/img/city.jpg' ?>" />
+                        <div class="text">
+                            <h1>Hej och välkommen!</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed sodales mauris. Aliquam felis est, efficitur vel fringilla quis, vehicula quis ex.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php get_footer() ?>
