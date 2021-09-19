64af28d71534bf0e555dce54849bd5e0d9967f48
Thomas Olsson <olss.thom@gmail.com>
2021-09-19 18:50:11 +0200

    gets front page content with the loop

--- front-page.php
+++ front-page.php
@@ -6,11 +6,19 @@
             <div class="row">
                 <div class="col-xs-12">
                     <div class="hero">
-                        <img src="<?php echo get_template_directory_uri()  . '/img/city.jpg' ?>" />
-                        <div class="text">
-                            <h1>Hej och välkommen!</h1>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed sodales mauris. Aliquam felis est, efficitur vel fringilla quis, vehicula quis ex.</p>
-                        </div>
+                        <img src="<?php echo get_template_directory_uri() . '/img/city.jpg' ?>" />
+
+                        <?php
+                        if (have_posts()) :
+                            while (have_posts()) : the_post(); ?>
+                                <div class="text">
+                                    <h1><?php the_title() ?></h1>
+                                    <p><?php the_content() ?></p>
+                                </div>
+                        <?php
+                            endwhile;
+                        endif; ?>
+
                     </div>
                 </div>
             </div>
