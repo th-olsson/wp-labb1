b328b6636957d4ad5361b79025b46da8246dba2e
Thomas Olsson <olss.thom@gmail.com>
2021-09-20 03:49:22 +0200

    uses the loop for to display content for pages

--- page-undersida-4.php
+++ page-undersida-4.php
@@ -5,13 +5,20 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-12 col-sm-8 col-md-6">
-					<h1>Undersida 4</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed sodales mauris. Aliquam felis est, efficitur vel fringilla quis, vehicula quis ex. Phasellus tristique nunc in leo faucibus, a consequat nulla sagittis. In sed mi mi. Praesent condimentum sollicitudin nibh. Vivamus vulputate purus quis volutpat fringilla. Ut tortor libero, semper eget dolor vel, hendrerit tempus dui. Suspendisse dictum efficitur blandit. In porta scelerisque nulla ac placerat.</p>
-					<p>Integer quis eros at urna pulvinar mollis eget quis ex. Nam fringilla tellus at ligula consequat, sed sagittis ipsum ultricies. Nunc pretium bibendum enim id iaculis. Nam sed leo non sem aliquam imperdiet dictum vitae magna. Phasellus ac accumsan dolor, quis congue mauris. Maecenas vehicula, arcu sed congue euismod, mi urna rhoncus nunc, et feugiat arcu tellus vitae odio. Sed luctus vel lorem vel dictum. Suspendisse potenti. Proin vel efficitur ex. Suspendisse scelerisque rutrum mattis. In sed laoreet augue, non maximus ante. Nam a rhoncus ligula. Aliquam at aliquet lorem. Suspendisse non risus tincidunt, gravida augue non, fringilla libero.</p>
-					<p>Sed pretium metus in urna vehicula imperdiet. Fusce at odio vitae felis maximus tincidunt. Nunc porttitor ex a elementum dictum. Maecenas eget arcu nulla. Cras auctor dui aliquet, condimentum orci vel, venenatis nisl. Ut semper magna urna, ac congue dui cursus vitae. Nam sed pharetra leo, vel tincidunt est. Phasellus volutpat tortor nec nulla feugiat congue. Donec quis ligula varius, euismod nisl eu, aliquet metus. Aliquam tempus iaculis odio, sed volutpat mi aliquam aliquet.</p>
+
+					<?php
+					if (have_posts()) :
+						while (have_posts()) : the_post(); ?>
+							<div class="text">
+								<h1><?php the_title() ?></h1>
+								<p><?php the_content() ?></p>
+							</div>
+					<?php
+						endwhile;
+					endif; ?>
 				</div>
 				<div class="col-xs-12 col-sm-4 col-md-6">
-					<img src="img/photo.jpg" />
+					<?php the_post_thumbnail() ?>
 				</div>
 			</div>
 		</div>
